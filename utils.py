4269384a25fc3b5f0a5496ac128b72ac91dd6acf
ChrisSanchezDev <ChrisSanchezDev@gmail.com>
2025-12-30 14:54:32 -0500

    Add scripts for auditing internships and jobs, including utility functions for Notion integration

---
+++ utils.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from notion_client import Client
+
+load_dotenv()
+
+def get_notion_client():
+    token = Client(auth=os.getenv('NOTION_TOKEN'))
+    if not token:
+        raise ValueError("CRITICAL ERROR: NOTION_TOKEN not found!")
+    return token
+
+def get_database_id(env_str):
+    db_id = os.getenv(env_str)
+    if not db_id:
+        raise ValueError(f"CRITICAL ERROR: {env_str} not found!")
+    return format_uuid(db_id) 
+
+def is_test_run():
+    test_run = os.getenv('TEST_RUN', 'true').lower() == 'true'
+    if test_run is True:
+        print("---TEST_RUN Enabled---")
+    return test_run
+
+def format_uuid(id_str):
+    if not id_str:
+        return None
+    
+    # Removes any -'s and whitespaces
+    clean = id_str.replace("-", "").strip()
+
+    # Slice it into the 5 standard groups to make the FULL database_id
+    return f"{clean[:8]}-{clean[8:12]}-{clean[12:16]}-{clean[16:20]}-{clean[20:]}"
